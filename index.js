13da14027c2c361a7920029a2e79eb8f7d1b3ddb
Victor Azevedo <victorazesc@hotmail.com>
2022-06-02 20:42:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// const puppeteer = require('puppeteer');
-
 const chromium = require('chrome-aws-lambda');
-
-const sleep = async (time) => new Promise((resolve) => setTimeout(resolve, time))
-
 
 async function votar () {
   let votos = 0
@@ -19,35 +14,25 @@ async function votar () {
       })
       const page = await browser.newPage();
       await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSe0PbHNk7nCs7NqTAzGh81fP1C2sCpJ1X3Nj-3-IREsbP6KGw/viewform');
-
       await page.waitForSelector('[data-value="Opção 9"]')
       await page.click('[data-value="Opção 9"]')
       await page.waitForSelector('[role="button"]')
       await page.click('[role="button"]')
 
       await page.evaluate(async (btnSelector) => {
-        // this executes in the page
         await document.querySelector(btnSelector).click();
       }, 'div[jsname="ksKsZd"]');
 
       await page.waitForSelector('.vHW8K')
-      // await page.screenshot({ path: `prints/example${votos}.png` });
       await browser.close();
       votos = votos + 1
       console.log(votos)
     } catch (error) {
       console.log(error.message)
-      await sleep(100)
     }
 
   } while (true)
 };
 
 
-
 votar()
-
-
-
-
-
