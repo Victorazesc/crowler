b609eb9e14afc2586529c82cd26c5305115f092e
Victor Azevedo <victorazesc@hotmail.com>
2022-06-02 20:42:21 -0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+@echo.
+@echo    *************************************************
+@echo    ***                                           ***
+@echo    ***  PARA PARAR A VOTACAO PRECIONE CTRL + C!  ***
+@echo    ***                                           ***
+@echo    *************************************************
+@echo.
+
+npm run start 
