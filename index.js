ad24530f34b95b817bd2d616447dc93c10e7a420
Victor Azevedo <victorazesc@hotmail.com>
2022-06-03 00:49:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,69 +10,44 @@ async function votar () {
   let votos = 0
 
 
-  try {
-    puppeteer.use(StealthPlugin())
-    const browser = await puppeteer.launch({ headless: process.argv.slice(2)[0] ? false : true })
-    const page = await browser.newPage();
 
-    console.log('logando no google...')
+  puppeteer.use(StealthPlugin())
+  const browser = await puppeteer.launch({ headless: process.argv.slice(2)[0] ? false : true })
+  const page = await browser.newPage();
 
-    await page.goto('https://accounts.google.com/signin/v2/identifier');
+  console.log('logando no google...')
 
-    await page.waitForSelector('[type="email"]')
-    await page.type('[type="email"]', 'crowlerazevedo@gmail.com')
-    await page.click('#identifierNext')
-    await page.waitForTimeout(1500)
-    await page.type('[type="password"]', 'corte99100wc')
-    await page.click('#passwordNext')
+  await page.goto('https://accounts.google.com/signin/v2/identifier');
 
-    await page.waitForTimeout(1500)
-    await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSe0PbHNk7nCs7NqTAzGh81fP1C2sCpJ1X3Nj-3-IREsbP6KGw/viewform');
+  await page.waitForSelector('[type="email"]')
+  await page.type('[type="email"]', 'crowlerazevedo@gmail.com')
+  await page.click('#identifierNext')
+  await page.waitForTimeout(1500)
+  await page.type('[type="password"]', 'corte99100wc')
+  await page.click('#passwordNext')
 
-    await page.waitForSelector('[data-value="Opção 9"]')
-    await page.click('[data-value="Opção 9"]')
-    await page.waitForSelector('[role="button"]')
-    await page.click('[role="button"]')
+  await page.waitForTimeout(2500)
 
-    votos = votos + 1
-    console.log(`###################\n\n ${votos} Votos!\n\n###################`)
+  do {
+    try {
+      await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSe0PbHNk7nCs7NqTAzGh81fP1C2sCpJ1X3Nj-3-IREsbP6KGw/viewform');
 
-    while (true) {
-      try {
-        await page.waitForSelector('[class="c2gzEf"]')
-        await page.click('a')
-        await page.waitForSelector('[data-value="Opção 9"]')
-        await page.click('[data-value="Opção 9"]')
-        await page.waitForSelector('[role="button"]')
-        await page.click('[role="button"]')
-
-        await page.evaluate(async (btnSelector) => {
-          await document.querySelector(btnSelector).click();
-        }, 'div[jsname="ksKsZd"]');
-
-        votos = votos + 1
-        console.clear()
-        console.log(`###################\n\n ${votos} Votos!\n\n###################`)
-
-
-
-      } catch (error) {
-        console.log(error.message)
-      }
-
+      await page.waitForSelector('[data-value="Opção 9"]')
+      await page.click('[data-value="Opção 9"]')
+      await page.waitForSelector('[role="button"]')
+      await page.click('[role="button"]')
+      await page.waitForSelector('.vHW8K')
+      votos = votos + 1
+      console.clear()
+      console.log(`###################\n\n ${votos} Votos!\n\n###################`)
+    } catch (error) {
+      console.log(error.message)
     }
-    // await browser.close();
+  } while (true)
 
-  } catch (error) {
-    console.log(error.message)
-  }
+}
 
-};
 
 
 votar()
-
-
-
-
 
